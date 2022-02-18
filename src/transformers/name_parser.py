fc38394d67d503405d892fa364751d279c61a923
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-19 00:09:11 +0200

    feat: add surname and title parser transformers

--- src/transformers/name_parser.py
+++ src/transformers/name_parser.py
@@ -1,26 +1,26 @@
 from typing import Tuple
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer, OneHotEncoder
 import numpy as np
 import pandas as pd
 
 @FunctionTransformer
-def parse_names(full_names: np.array) -> np.array:
-    @np.vectorize
-    def _parse_names(full_name: str) -> Tuple[str, str]:
-        """
-            Extracts surname and title.
+def parse_surnames(full_names: np.array) -> np.array:
+    _parse_surnames = np.vectorize(lambda full_name: full_name.split(", ")[0])
+    return _parse_surnames(full_names)
 
-        Example:
-            'Braund, Mr. Owen Harris' -> ["Braund", "Mr"]
-        """
-        surname, name = full_name.split(", ")
-        title = name.split(". ")[0]
-        return (surname, title)
-    return _parse_names(full_names)
-
+@FunctionTransformer
+def parse_titles(full_name: np.array) -> np.array:
+    _parse_titles = np.vectorize(lambda full_name: full_name.split(", ")[1].split(" ")[0])
+    return _parse_titles(full_name)
 
 if __name__ == "__main__":
     data = pd.read_csv("src\\datasets\\train.csv")["Name"]
-    parse_names.fit_transform(data.values[:2])
-    
-    # (array(['Braund', 'Cumings'], dtype='<U7'), array(['Mr', 'Mrs'], dtype='<U3'))
+    print(data.values[:2])
+    print(parse_surnames.fit_transform(data.values[:2]))
+    print(parse_titles.fit_transform(data.values[:2]))
+"""
+['Braund, Mr. Owen Harris' 'Cumings, Mrs. John Bradley (Florence Briggs Thayer)']
+['Braund' 'Cumings']
+['Mr.' 'Mrs.']
+"""
