9192db7b15b6ea2de96e3ed44ba181bff9b75562
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-18 21:19:56 +0200

    feat: fit adaBoost and XGBoost algorithms

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cProfile import run
 import runpy
 
-runpy.run_module("src.pipelines.pipeline_001", run_name="__main__")
-runpy.run_module("src.pipelines.pipeline_002", run_name="__main__")
+# runpy.run_module("src.pipelines.pipeline_001", run_name="__main__")
+# runpy.run_module("src.pipelines.pipeline_002", run_name="__main__")
 runpy.run_module("src.models.xgboost_pipeline_002")
+runpy.run_module("src.models.adaboost_pipeline_002")
--- src/models/xgboost_pipeline_002.py
+++ src/models/xgboost_pipeline_002.py
@@ -1,31 +1,26 @@
 import pandas as pd
 import xgboost as xgb 
-from sklearn.model_selection import GridSearchCV
 
 from src.pipelines.pipeline_002 import pipeline
-from src.scripts.gridsearch_cv_info import print_gridsearch_cv_results
+from src.models.model_builder import ModelBuilder
 
 import warnings
 warnings.filterwarnings('ignore')
 
 data = pd.read_csv("src\\datasets\\train.csv")
-X_train, y_train = pd.DataFrame(pipeline.fit_transform(data), index=data.PassengerId), data.Survived
+test_data = pd.read_csv("src\\datasets\\test.csv")
 
-params = { 'max_depth': [3, 6, 10, 15],
+parameters = { 'max_depth': [3, 6, 10, 15],
            'learning_rate': [0.01, 0.05, 0.1],
            'n_estimators': [10, 30, 50],
            'colsample_bytree': [0.3, 0.7]}
 
-xgbr = xgb.XGBClassifier(seed = 20)
-clf = GridSearchCV(estimator=xgbr, 
-                   param_grid=params,
-                   scoring="roc_auc", 
-                   verbose=10)
+xgb_classifier = xgb.XGBClassifier(seed = 20)
 
-clf.fit(X_train, y_train)
-print_gridsearch_cv_results(clf)
+model = ModelBuilder(pipeline=pipeline, estimator=xgb_classifier, tuning_parameters=parameters)
+model.tune_parameters(data=data, metric="roc_auc", verbose=10)
+model.predict(test_data).to_csv(f"src\\predictions\\{model.get_model_score():.2f}_xgboost_pipeline_02_01.csv")
 
-test_data = pd.read_csv("src\\datasets\\test.csv")
-model = clf.best_estimator_
-prediction = pd.DataFrame(model.predict(pipeline.transform(test_data)), columns=["Survived"], index=test_data.PassengerId)
-prediction.to_csv("src\\predictions\\xgboost_pipeline_002.csv")
+print(model.get_model_score())
+print(model.get_model_params())
+
