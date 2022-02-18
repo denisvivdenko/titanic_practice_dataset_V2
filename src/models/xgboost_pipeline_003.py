8fc07107c39df0774774dc0134e626d06994eea0
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-19 00:56:11 +0200

    feat: tune xgboost on pipeline_003

---
+++ src/models/xgboost_pipeline_003.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import xgboost as xgb 
+
+from src.pipelines.pipeline_003 import pipeline
+from src.models.model_builder import ModelBuilder
+
+import warnings
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv("src\\datasets\\train.csv")
+test_data = pd.read_csv("src\\datasets\\test.csv")
+
+parameters = { 'max_depth': [3, 6, 10, 15],
+           'learning_rate': [0.01, 0.05, 0.1],
+           'n_estimators': [10, 30, 50],
+           'eval_metric': ['auc'],
+           'alpha': [2, 5, 10]}
+
+xgb_classifier = xgb.XGBClassifier(seed = 20)
+
+model = ModelBuilder(pipeline=pipeline, estimator=xgb_classifier, tuning_parameters=parameters)
+model.tune_parameters(data=data, metric="roc_auc", verbose=10)
+model.predict(test_data).to_csv(f"src\\predictions\\{model.get_model_score():.2f}_xgboost_pipeline_03_01.csv")
+
+print(model.get_model_score())
+print(model.get_model_params())
+
